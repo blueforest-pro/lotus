5e8f0bc6471fc4990eabd8e59e0f259fe84f20f9
_blue_forest_ <ht706204@163.com>
2021-02-24 19:38:09 +0800

    更改任务分配

--- extern/sector-storage/sched.go
+++ extern/sector-storage/sched.go
@@ -745,7 +745,8 @@ func (sh *scheduler) getTaskFreeCount(wid WorkerID, phaseTaskType sealtasks.Task
 	}
 
 	whl := sh.workers[wid]
-	log.Infof("mydebug:worker:%s,take_type:%s,free:%d", whl.info.Hostname, phaseTaskType, freeCount)
+	log.Infof("mydebug:getTaskFreeCount:%s,take_type:%s,limit:%d,runCount:%d,free:%d",
+		whl.info.Hostname, phaseTaskType, limitCount, runCount, freeCount)
 
 	if phaseTaskType == sealtasks.TTAddPiece || phaseTaskType == sealtasks.TTPreCommit1 {
 		if freeCount >= 0 { // 空闲数量不小于0，小于0也要校准为0
