25c6ae2958586f9201f57c8e2fb5e0c04d5d6338
_blue_forest_ <ht706204@163.com>
2021-03-09 21:41:09 +0800

    更改任务计数和IP限制

--- extern/sector-storage/storiface/worker.go
+++ extern/sector-storage/storiface/worker.go
@@ -181,7 +181,7 @@ type taskLimitConfig struct {
 
 func NewTaskLimitConfig() map[sealtasks.TaskType]*TaskConfig {
 	config := &taskLimitConfig{
-		AddPiece:     1,
+		AddPiece:     2,
 		PreCommit1:   7,
 		PreCommit2:   8,
 		Commit1:      8,
